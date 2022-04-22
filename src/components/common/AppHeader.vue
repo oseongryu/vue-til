5e2a672614013aa2d7cb47178e6edefaa8a00e8f
oseongryu <oseongryu@gmail.com>
2022-04-22 09:04:19 +0900

    feat: router

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>header</div>
+	<div>header</div>
 </template>
 
 <script>
-// import Demo from '../../demo/basic/Demo'
-import Demo from '@/demo/basic/Demo';
+import Demo from '../../demo/basic/Demo'
+import Demo from '@/demo/basic/Demo'
+// import Demo from '@/demo/basic/Demo'
 
 export default {};
 </script>
